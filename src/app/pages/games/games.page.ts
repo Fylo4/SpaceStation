7808d76b54661f66fde406ae98201b21d7ae50c9
Philip Sawyer <philipsawyer2@gmail.com>
2025-05-12 17:00:15 -0700

    Improved display for game rules

--- src/app/pages/games/games.page.ts
+++ src/app/pages/games/games.page.ts
@@ -1,12 +1,14 @@
 import { Component } from "@angular/core";
 import { RoutePaths } from "../../app.routes";
+import { MatExpansionModule } from '@angular/material/expansion'
+import { MatTabsModule } from '@angular/material/tabs'
 
 @Component({
     selector: 'app-games-page',
     templateUrl: 'games.page.html',
     styleUrl: 'games.page.scss',
     standalone: true,
-    imports: []
+    imports: [MatExpansionModule, MatTabsModule]
 })
 export class GamesPage {
     routes = RoutePaths;
